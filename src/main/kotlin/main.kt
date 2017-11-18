fd19202bc80d22ecb5c3017ae2db835004701747
Victor Kropp <victor.kropp@gmail.com>
2017-11-18 23:21:22 +0100

    3.1 DSL: calendar

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,10 @@
 import biweekly.Biweekly
 import biweekly.ICalendar
 import biweekly.component.VEvent
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.pipeline.PipelineContext
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -14,19 +16,20 @@ fun main(args: Array<String>) {
   val server = embeddedServer(Netty, 8080) {
     routing {
       get("/") {
-        val ical = ICalendar()
-        val event = VEvent()
-        event.setSummary("Christmas")
+        calendar {
+          val event = VEvent()
+          event.setSummary("Christmas")
 
-        val date = GregorianCalendar(2017, 11, 25).time
-        event.setDateStart(date)
-        event.setDateEnd(date)
+          val date = GregorianCalendar(2017, 11, 25).time
+          event.setDateStart(date)
+          event.setDateEnd(date)
 
-        ical.addEvent(event)
-
-        call.respondText(Biweekly.write(ical).go(), ContentType("text", "calendar"))
+          addEvent(event)
+        }
       }
     }
   }
   server.start(wait = true)
 }
+
+suspend fun PipelineContext<Unit, ApplicationCall>.calendar(builder: ICalendar.() -> Unit) = call.respondText(Biweekly.write(ICalendar().apply(builder)).go(), ContentType.Text.Plain)
