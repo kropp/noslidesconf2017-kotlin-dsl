72aadc1a77cecec307e792ccedfe96005294844e
Victor Kropp <victor.kropp@gmail.com>
2017-11-18 23:19:24 +0100

    2. calendar example

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,30 @@
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import biweekly.Biweekly
+import biweekly.ICalendar
+import biweekly.component.VEvent
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.util.*
 
 fun main(args: Array<String>) {
   val server = embeddedServer(Netty, 8080) {
     routing {
       get("/") {
-        call.respondText("Hello, NoSlidesConf!", ContentType.Text.Html)
+        val ical = ICalendar()
+        val event = VEvent()
+        event.setSummary("Christmas")
+
+        val date = GregorianCalendar(2017, 11, 25).time
+        event.setDateStart(date)
+        event.setDateEnd(date)
+
+        ical.addEvent(event)
+
+        call.respondText(Biweekly.write(ical).go(), ContentType("text", "calendar"))
       }
     }
   }
